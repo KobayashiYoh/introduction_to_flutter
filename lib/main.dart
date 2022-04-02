23a4ccd37875257b1d596fe561a578c61a3346de
Yo Kobayashi <y.kobayashi@4nonome.com>
2022-04-02 14:20:15 +0900

    create data class

--- lib/main.dart
+++ lib/main.dart
@@ -101,8 +101,152 @@ class Page1 extends StatefulWidget {
 }
 
 class _Page1State extends State<Page1> {
+  Widget tweetHeader(Tweet tweet) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Row(
+          children: [
+            Text(
+              tweet.userName,
+              style: const TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ), // TextStyle
+            ), // Text
+            const SizedBox(width: 8.0),
+            Text('@${tweet.userId}'),
+            const Text('・1時間'),
+          ],
+        ),
+        const Icon(Icons.rectangle),
+      ], // <Widget>[]
+    ); // Row
+  }
+
+  Widget tweetFooter(Tweet tweet) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        tweetFooterItem(Icons.rectangle, tweet.replayCount), // リプライ
+        tweetFooterItem(Icons.rectangle, tweet.retweetCount), // リツイート
+        tweetFooterItem(Icons.rectangle, tweet.likesCount), // いいね
+        const Icon(Icons.rectangle), // 共有
+        const SizedBox.shrink(),
+      ], // <Widget>[]
+    ); // Row
+  }
+
+  Widget tweetFooterItem(IconData iconData, int count) {
+    return Row(
+      children: <Widget>[
+        Icon(iconData),
+        const SizedBox(width: 8.0),
+        Text(count.toString()),
+      ], // <Widget>[]
+    ); // Row
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: Data.tweetList.length,
+        shrinkWrap: true,
+        itemBuilder: (BuildContext context, int index) => Column(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 24.0,
+                    backgroundImage:
+                        NetworkImage(Data.tweetList[index].userIconUrl),
+                  ), // CircleAvatar
+                  const SizedBox(width: 8.0),
+                  Flexible(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        tweetHeader(Data.tweetList[index]), // ツイートのHeader
+                        Text(Data.tweetList[index].text), // ツイートのBody
+                        tweetFooter(Data.tweetList[index]), // ツイートのFooter
+                      ], // <Widget>[]
+                    ), // Column
+                  ), // Flexible
+                ], // <Widget>[]
+              ), // Row
+            ), // Container
+            const Divider(),
+          ], // <Widget>[]
+        ), // Column
+      ), // ListView.builder
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        elevation: 0,
+        child: const Icon(
+          Icons.add,
+          size: 32.0,
+        ), // Icon
+      ), // FloatingActionButton
+    ); // Scaffold
+  }
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class Page3 extends StatelessWidget {
+  const Page3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class Page4 extends StatelessWidget {
+  const Page4({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class Tweet {
+  String userIconUrl;
+  String userName;
+  String userId;
+  String text;
+  String postImage;
+  int replayCount;
+  int retweetCount;
+  int likesCount;
+
+  Tweet({
+    required this.userIconUrl,
+    required this.userName,
+    required this.userId,
+    required this.text,
+    required this.postImage,
+    required this.replayCount,
+    required this.retweetCount,
+    required this.likesCount,
+  });
+}
+
+class Data {
   // ------------------------- ツイートのデータ -------------------------
-  List<Tweet> tweetList = [
+  static List<Tweet> tweetList = [
     Tweet(
       userIconUrl:
           'https://pbs.twimg.com/media/FPTPnEvVQAQ6C9z?format=jpg&name=360x360',
@@ -215,144 +359,4 @@ class _Page1State extends State<Page1> {
     ),
   ]; // ------------------------- ツイートのデータ -------------------------
 
-  Widget tweetHeader(Tweet tweet) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Row(
-          children: [
-            Text(
-              tweet.userName,
-              style: const TextStyle(
-                fontSize: 16.0,
-                fontWeight: FontWeight.bold,
-              ), // TextStyle
-            ), // Text
-            const SizedBox(width: 8.0),
-            Text('@${tweet.userId}'),
-            const Text('・1時間'),
-          ],
-        ),
-        const Icon(Icons.rectangle),
-      ], // <Widget>[]
-    ); // Row
-  }
-
-  Widget tweetFooter(Tweet tweet) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        tweetFooterItem(Icons.rectangle, tweet.replayCount), // リプライ
-        tweetFooterItem(Icons.rectangle, tweet.retweetCount), // リツイート
-        tweetFooterItem(Icons.rectangle, tweet.likesCount), // いいね
-        const Icon(Icons.rectangle), // 共有
-        const SizedBox.shrink(),
-      ], // <Widget>[]
-    ); // Row
-  }
-
-  Widget tweetFooterItem(IconData iconData, int count) {
-    return Row(
-      children: <Widget>[
-        Icon(iconData),
-        const SizedBox(width: 8.0),
-        Text(count.toString()),
-      ], // <Widget>[]
-    ); // Row
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.builder(
-        itemCount: tweetList.length,
-        shrinkWrap: true,
-        itemBuilder: (BuildContext context, int index) => Column(
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  CircleAvatar(
-                    radius: 24.0,
-                    backgroundImage: NetworkImage(tweetList[index].userIconUrl),
-                  ), // CircleAvatar
-                  const SizedBox(width: 8.0),
-                  Flexible(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        tweetHeader(tweetList[index]), // ツイートのHeader
-                        Text(tweetList[index].text), // ツイートのBody
-                        tweetFooter(tweetList[index]), // ツイートのFooter
-                      ], // <Widget>[]
-                    ), // Column
-                  ), // Flexible
-                ], // <Widget>[]
-              ), // Row
-            ), // Container
-            const Divider(),
-          ], // <Widget>[]
-        ), // Column
-      ), // ListView.builder
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        elevation: 0,
-        child: const Icon(
-          Icons.add,
-          size: 32.0,
-        ), // Icon
-      ), // FloatingActionButton
-    ); // Scaffold
-  }
-}
-
-class Page2 extends StatelessWidget {
-  const Page2({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
-class Page3 extends StatelessWidget {
-  const Page3({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
-class Page4 extends StatelessWidget {
-  const Page4({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
-class Tweet {
-  String userIconUrl;
-  String userName;
-  String userId;
-  String text;
-  String postImage;
-  int replayCount;
-  int retweetCount;
-  int likesCount;
-
-  Tweet({
-    required this.userIconUrl,
-    required this.userName,
-    required this.userId,
-    required this.text,
-    required this.postImage,
-    required this.replayCount,
-    required this.retweetCount,
-    required this.likesCount,
-  });
 }
